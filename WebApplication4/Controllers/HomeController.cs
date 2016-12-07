60049674b822a2992ac20a0a48d993be7216122d
Mark Dewey <mark.t.dewey@gmail.com>
2016-12-07 16:30:04 -0500

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4.Models;
+
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public static List<Person> People { get; set; } = new List<Person>
+        {
+            new Person { FavoriteMovie = "Goldfinger", Name="Paul" },
+            new Person { FavoriteMovie = "Boondock Saints", Name="Ryan" },
+            new Person {FavoriteMovie="Death Proof", Name="Uma" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Person> GetAllPeople()
+        {
+            return People;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetPerson(string name)
+        {
+            return Ok(People.FirstOrDefault(f => String.Compare(name, f.Name) == 0));
+        }
+        [HttpPut]
+        public Person PutPerson(string name, string movie)
+        {
+            var p = new Person { Name = name, FavoriteMovie = movie };
+            People.Add(p);
+            return p;
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostPeron(Person updated)
+        {
+            var found = People.FirstOrDefault(f => f.Id == updated.Id);
+            if (found == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                found.Name = updated.Name;
+                found.FavoriteMovie = updated.FavoriteMovie;
+                return Ok(found);
+            }
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeletePerson(Guid id)
+        {
+            People = People.Where(w => w.Id != id).ToList();
+            return Ok();
+        }
+
+
+
+
+    }
+}
---
+++ WebApplication4/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string FavoriteMovie { get; set; }
+    }
+}
